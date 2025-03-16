e5c4161dcaa0efb86a9d67e536522463254d0790
jkalbhenn <jkalbhenn@intera.de>
2025-03-16 21:24:10 +0100

    extend routes for the plugin to run

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 get "time_logging_app", to: "time_logging_app#index"
 get "time_logging_app/show", to: "time_logging_app#show"
 get "time_logging_app/edit", to: "time_logging_app#edit"
-match "time_logging_app/time_entries", to: "time_logging_app#time_entries", via: [:post, :put, :delete]
+match "time_logging_app/time_entries", to: "time_logging_app#time_entries", via: [:get, :post, :put, :delete]
+
+get "time_logging_app/projects_and_issues", to: "time_logging_app#projects_and_issues"
+get "time_logging_app/recent", to: "time_logging_app#recent"
+get "time_logging_app/spent_time", to: "time_logging_app#spent_time"
+get "time_logging_app/overview", to: "time_logging_app#overview"
